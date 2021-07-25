d98275fb30c55e2b224c14bdf653c08c9c475111
Victor Miranda Balduino <66287562+VictorRandom@users.noreply.github.com>
2021-07-25 17:51:10 -0300

    Update problem9.js

--- Project_Euler/problem9.js
+++ Project_Euler/problem9.js
@@ -1,3 +1,8 @@
+/*A Pythagorean triplet is a set of three natural numbers, <var>a ; b ; c, for which,
+ a² + b² = c²
+For example, 3² + 4² = 9 + 16 = 25 = 5².
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.Find the product abc.*/
+
 var a = 1
 var b = 2
 var c = Math.sqrt(a*a + b*b)
