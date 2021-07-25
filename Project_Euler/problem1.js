9a1d9ac48603b8abc0b5b956cee8ee14b747672a
Victor Miranda Balduino <66287562+VictorRandom@users.noreply.github.com>
2021-07-25 17:45:51 -0300

    Update problem1.js

--- Project_Euler/problem1.js
+++ Project_Euler/problem1.js
@@ -1,3 +1,6 @@
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
 var soma = 0
 for ( var n = 1 ; n < 1000 ; n++){
     if ( n%5 == 0 || n%3 == 0 ){    
