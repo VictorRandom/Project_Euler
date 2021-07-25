cf5ac447c815f3a3991fc70be0a4a5994afa8303
Victor Miranda Balduino <66287562+VictorRandom@users.noreply.github.com>
2021-07-25 17:49:52 -0300

    Update problem5.js

--- Project_Euler/problem5.js
+++ Project_Euler/problem5.js
@@ -1,3 +1,7 @@
+//2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+//What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+
 function eDivisivel(numero)
 {
     for(divisor = 2; divisor <= 20; divisor ++){
